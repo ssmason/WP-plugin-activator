57d816dc678f790943dbb2e9b817a1e3c7df8c8a
stephen mason <stephenmason@stephens-MBP.OleLan>
2025-10-06 17:12:55 +0200

    chore: restructure for mu-plugins | add config inside plugin

--- satori-wp-plugin-activator.php
+++ satori-wp-plugin-activator.php
@@ -6,10 +6,17 @@
  * @package  SatoriDigital\PluginActivator
  */
 
-if ( file_exists( dirname( __FILE__ ) . '/vendor/autoload.php' ) ) { 
-	include_once dirname( __FILE__ ) . '/vendor/autoload.php';
+
+if ( ! defined( 'PLUGIN_ACTIVATION_CONFIG' ) ) {
+	define( 'PLUGIN_ACTIVATION_CONFIG', __DIR__ . '/satori-wp-plugin-activator/config' );
 }
 
-use SatoriDigital\PluginActivator\Activate; 
-$activate = new Activate();
-$activate->run();
+if ( file_exists( __DIR__ . '/satori-wp-plugin-activator/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/satori-wp-plugin-activator/vendor/autoload.php';
+}
+
+use SatoriDigital\PluginActivator\Controllers\ActivationController;
+
+add_action( 'after_setup_theme', function() {
+	( new ActivationController() )->run();
+});
